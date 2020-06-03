5d334aba03a9824c4f28d652641322ed9e905437
Victor Dominguez <vm.dominguez@alumnos.upm.es>
2020-06-03 18:00:55 +0200

    Borrador inicial

---
+++ src/pruebas.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
+
+for f in list_ds.take(5):
+  print(f.numpy())
+
+def decode_img(img):
+  # convert the compressed string to a 3D uint8 tensor
+  img = tf.image.decode_jpeg(img, channels=3)
+  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  # resize the image to the desired size.
+  return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
+
+def process_path(file_path):
+  label = get_label(file_path)
+  # load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+for image, label in labeled_ds.take(1):
+  print("Image shape: ", image.numpy().shape)
+  print("Label: ", label.numpy())
+
+def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
+  # This is a small dataset, only load it once, and keep it in memory.
+  # use `.cache(filename)` to cache preprocessing work for datasets that don't
+  # fit in memory.
+  if cache:
+    if isinstance(cache, str):
+      ds = ds.cache(cache)
+    else:
+      ds = ds.cache()
+
+  ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+
+  # Repeat forever
+  ds = ds.repeat()
+
+  ds = ds.batch(BATCH_SIZE)
+
+  # `prefetch` lets the dataset fetch batches in the background while the model
+  # is training.
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
+
+  return ds
+
